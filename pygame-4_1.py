b3181c8990fde3f98dd5a76480be0e2bf245aef2
N.Anikeev <ankr_21@mail.ru>
2024-06-11 02:12:52 +0300

    лаба 4.3

--- pygame-4_1.py
+++ pygame-4_1.py
@@ -103,6 +103,13 @@ class Bullet(pygame.sprite.Sprite):
         if self.rect.bottom < 0:
             self.kill()
 
+font_name = pygame.font.match_font('arial')
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
 
 background = pygame.image.load(path.join(img_dir, "bg_1_1.png")).convert()
 background_rect = background.get_rect()
@@ -126,6 +133,7 @@ for i in range(8):
     all_sprites.add(m)
     mobs.add(m)
 
+score = 0
 running = True
 while running:
     clock.tick(FPS)
@@ -140,6 +148,7 @@ while running:
 
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
     for hit in hits:
+        score += 50 - hit.radius
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
@@ -151,6 +160,7 @@ while running:
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
+    draw_text(screen, str(score), 18, WIDTH / 2, 10)
 
     pygame.display.flip()
 
